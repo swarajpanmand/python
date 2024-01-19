ed21e524fafc080c16ba1451918c24c44b609011
Swaraj Panmand <swarajpanmand@gmail.com>
2024-01-20 01:08:38 +0530

    second file

---
+++ geeksforgeeks/max_two.py
@@ -0,0 +1,10 @@
+def max(num1,num2):
+    if num1>num2:
+        return num1
+    else:
+        return num2
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(max(num1,num2))
+
