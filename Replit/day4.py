7625663717d3068dbff3d927facbe3c5c2646275
Swaraj Panmand <swarajpanmand@gmail.com>
2024-02-02 23:38:13 +0530

    day4

---
+++ Replit/day4.py
@@ -0,0 +1,12 @@
+print("=== Your adventure simulator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+story with YOU as the star! 🤩""")
+print()
+name = input("Your name: ")
+enemy = input("Your worst enemy's name: ")
+superpower = input("Your super power: ")
+print()
+print("Our story begins as pur hero name approaches a foreboding castle...")
+print("suddenly a bolt of lightning striked the ground at the feet of", name)
+print("\033[31m'Our final battle begins!'\033[0m shouts the evil", enemy , "clearly missing the fact that",name, "has the power of\033[35m", superpower, "\033[0mwhich means they'll win quite easily")
