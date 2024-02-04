4a1ed4f9570f347dc2d8dbeaa0589fc4d1407378
Swaraj Panmand <swarajpanmand@gmail.com>
2024-02-04 19:22:24 +0530

    day5

---
+++ Replit/day5.py
@@ -0,0 +1,22 @@
+print("""
+Marvel Movie Character Creater
+--
+
+""")
+spiderman= input("Do you like hanging around?: ")
+if spiderman == "yes":
+  print("You are spiderman")
+else:
+  print("Then you're not Spider-man")
+  Korg= input("Do you like gravelly voice?: ")
+  if Korg == "yes":
+    print("Found you, Korg")
+  else:
+    print("Aww, you're not Korg")
+    Captain= input("Do you often feek marvelous?: ")
+    if Captain == "yes":
+      print("Aha! You're Captain Marvel! Hi!")
+    else:
+      print("YOU SHOULD START WATCHING MOVIES BRO!!")
+
+
