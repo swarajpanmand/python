47638756dd3070ed96de98ebe6b6bb06d6331d2f
Swaraj Panmand <swarajpanmand@gmail.com>
2024-02-06 18:27:10 +0530

    day7

---
+++ Replit/day7.py
@@ -0,0 +1,16 @@
+print("""
+Fake Fan Finder
+---------------""")
+anime = input("What's your favourite Anime?")
+if anime == "One piece":
+  print("Oh really?! Name me any of the characters?")
+  character = input()
+  if character == "Nami":
+    print("You got that by pure chance. Okay then, what is her job on the ship?")
+    job = input()
+    if job == "Navigator":
+      print("Hmph! What was her first bounty then?")
+      bounty = input()
+      if bounty == "Ummm...":
+        print("See! FAKE One piece fan!")
+        
