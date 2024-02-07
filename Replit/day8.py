0f2a73e6c6f1d9898d2fbcedf766ac6b06ced7d6
Swaraj Panmand <swarajpanmand@gmail.com>
2024-02-07 17:48:00 +0530

    day8

---
+++ Replit/day8.py
@@ -0,0 +1,16 @@
+print("Mean Old Insult Machine")
+print("Who are you?")
+name = input()
+if name =="David" or name == "david":
+  print("How old are you?")
+  age = int(input())
+  if age == 30:
+    print("You look like a 30")
+  hair= input("Do you have hair on your head?")
+  if hair == "yes" or hair == "Yes":
+    print("You have hair on your head")
+    scale=input("On a scale of 1-10 how bald are you?")
+    if scale == "1" or scale == "2" or scale == "3" or scale == "4":
+      print("You are bald")
+  else:
+    print("You don't have hair on your head")
