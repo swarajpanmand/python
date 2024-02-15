f6d9be96ae41ae464a9d64239fba44d424fc7073
Swaraj Panmand <swarajpanmand@gmail.com>
2024-02-15 17:32:27 +0530

    day16

---
+++ Replit/day16.py
@@ -0,0 +1,9 @@
+print("fill in the lyrics!")
+print("(type in the blanks lyrics, see if you're as coool as me)")
+print()
+count = 0
+user_input = ""
+while user_input !="give":
+  user_input = input("Never going to ______ you up.")
+  count+=1
+print("Well done! It only took you", count, "attempts.")
