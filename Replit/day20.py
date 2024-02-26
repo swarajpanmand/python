9f349b7bd33779c7a70b66da8b86bbaa4b936522
Swaraj Panmand <swarajpanmand@gmail.com>
2024-02-27 01:43:45 +0530

    day20

---
+++ Replit/day20.py
@@ -0,0 +1,8 @@
+print("List Generator")
+print()
+n= int(input("Start at: "))
+m= int(input("End before: "))
+s= int(input("Increment between values: "))
+print()
+for i in range(n, m, s):
+  print(i)
