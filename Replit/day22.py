25dbce9d9a766cff165bf6fa64e7e7a407437165
Swaraj Panmand <swarajpanmand@gmail.com>
2024-03-01 00:09:08 +0530

    day22

---
+++ Replit/day22.py
@@ -0,0 +1,17 @@
+import random
+
+print("one-million-to-one")
+
+number = random.randint(1,1000000)
+while True:
+    guess = int(input("What is your guess?: "))
+    if guess < number:
+      print("Too low")
+    elif guess > number:
+      print("Too high")
+    elif guess == number:
+      print("Correct")
+      break
+    else:
+      print("That is not the number I am looking for")
+
