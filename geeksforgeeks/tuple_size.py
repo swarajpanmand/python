3c40e2611f572e36d1561a894ab4c01b03d86cbe
Swaraj Panmand <swarajpanmand@gmail.com>
2024-03-11 12:49:25 +0530

    tuple

---
+++ geeksforgeeks/tuple_size.py
@@ -0,0 +1,9 @@
+import sys
+
+Tuple1 = ("A", 1, "B", 2, "C", 3)
+Tuple2 = ("Geek1", "Raju", "Geek2", "Nikhil", "Geek3", "Deepanshu")
+Tuple3 = ((1, "Lion"), ( 2, "Tiger"), (3, "Fox"), (4, "Wolf"))  
+
+print("Size of tuple1: " , str(sys.getsizeof(Tuple1)) , "bytes" )
+print("SIze of tuple2: " , str(sys.getsizeof(Tuple2)), "bytes")
+print("Size of tuple3: " , str(sys.getsizeof(Tuple3)), "bytes")
