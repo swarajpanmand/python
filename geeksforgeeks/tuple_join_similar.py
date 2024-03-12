940a438984b1c82fe9355a4fed834bf9f9a43d76
Swaraj Panmand <swarajpanmand@gmail.com>
2024-03-12 18:47:00 +0530

    tuple

---
+++ geeksforgeeks/tuple_join_similar.py
@@ -0,0 +1,17 @@
+test_list = [(5, 6), (5, 7), (6, 8), (6, 10), (7, 13)]
+lent = len(test_list)
+res=[]
+x= []
+
+for i in test_list:
+    if i[0] not in x:
+        x.append(i[0])
+for i in x:
+    p=[]
+    p.append(i)
+    for j in test_list:
+        if i == j[0]:
+            p.append(j[1])
+    res.append(p)
+
+print(res)
